3ec626569fadc36fcc59f3f9783d070e8ddaf10d
Rodrigo Bernardino <rodgober7@hotmail.com>
2020-12-10 22:31:20 -0600

    Construccion del build

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto es una prueba.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rodgober.github.io/Mosaicos_magicos/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mosaicos mágicos
         </a>
       </header>
     </div>
